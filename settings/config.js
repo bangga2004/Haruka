c87b5eb45c80be141a5422bab7931b71ba12bcdb
bangga2004 <96578070+bangga2004@users.noreply.github.com>
2022-02-25 15:38:28 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // self or public
-global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+global.self = true //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="𝐟𝐢𝐫𝐚𝐚"
-global.ownernumber = "13022814650"
-global.botname = "𝐟𝐢𝐫𝐚𝐚ʙᴏᴛ"
-global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
+global.ownername ="𝘽𝘼𝙍𝙊𝙉 𝙎𝙏𝙊𝙍𝙀᭄"
+global.ownernumber = "6282230397507"
+global.botname = "𝘽𝘼𝙍𝙊𝙉 𝙎𝙏𝙊𝙍𝙀᭄"
+global.thumbnail = fs.readFileSync("./settings/somp.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
 		free:20,
