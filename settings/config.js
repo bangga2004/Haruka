4fdc7dd4eab28ff08294f02d86fc34457f920423
bangga2004 <96578070+bangga2004@users.noreply.github.com>
2022-03-10 14:09:30 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -6,7 +6,7 @@ global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
 global.ownername ="YANZZ"
-global.ownernumber = "6282230397507"
+global.ownernumber = "6282230397507 NOMOR KAMU"
 global.botname = "YANZZ"
 global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
